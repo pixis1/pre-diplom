76ea523a29761056a01a81298319d14d2008afb4
pixis1 <80167203+pixis1@users.noreply.github.com>
2024-02-26 09:45:00 +0700

    Add files via upload

---
+++ Примеры/testdata.sql
@@ -0,0 +1,80 @@
+insert into MAP (ID_MAP, NAME_MAP, TIME_MAP, LEGHT_MAP, SPEED_MAP) values (15, '9', '1774-5-26 12:31:53', '2', 'C');
+
+insert into MAP (ID_MAP, NAME_MAP, TIME_MAP, LEGHT_MAP, SPEED_MAP) values (11, 'A', '464-12-25 3:15:20', '3', 'D');
+
+insert into MAP (ID_MAP, NAME_MAP, TIME_MAP, LEGHT_MAP, SPEED_MAP) values (0, 'B', '578-5-20 3:52:33', '4', 'E');
+
+insert into MAP (ID_MAP, NAME_MAP, TIME_MAP, LEGHT_MAP, SPEED_MAP) values (3, 'C', '1347-9-5 9:3:39', '5', 'F');
+
+insert into MAP (ID_MAP, NAME_MAP, TIME_MAP, LEGHT_MAP, SPEED_MAP) values (8, 'D', '870-1-5 7:3:51', '6', 'G');
+
+insert into MAP (ID_MAP, NAME_MAP, TIME_MAP, LEGHT_MAP, SPEED_MAP) values (13, 'E', '1708-8-1 11:47:57', '7', 'H');
+
+insert into MAP (ID_MAP, NAME_MAP, TIME_MAP, LEGHT_MAP, SPEED_MAP) values (9, 'F', '712-6-24 5:37:28', '8', 'I');
+
+insert into MAP (ID_MAP, NAME_MAP, TIME_MAP, LEGHT_MAP, SPEED_MAP) values (16, 'G', '416-1-14 2:48:5', '9', ' ');
+
+insert into MAP (ID_MAP, NAME_MAP, TIME_MAP, LEGHT_MAP, SPEED_MAP) values (10, 'H', '1614-5-31 11:4:39', 'A', '0');
+
+insert into MAP (ID_MAP, NAME_MAP, TIME_MAP, LEGHT_MAP, SPEED_MAP) values (7, 'I', '1855-7-27 13:6:53', 'B', '1');
+
+insert into MAP (ID_MAP, NAME_MAP, TIME_MAP, LEGHT_MAP, SPEED_MAP) values (5, ' ', '290-12-16 2:0:32', 'C', '2');
+
+insert into MAP (ID_MAP, NAME_MAP, TIME_MAP, LEGHT_MAP, SPEED_MAP) values (1, '0', '1134-8-9 7:54:19', 'D', '3');
+
+insert into MAP (ID_MAP, NAME_MAP, TIME_MAP, LEGHT_MAP, SPEED_MAP) values (14, '1', '1-1-1 0:0:0', 'E', '4');
+
+insert into MAP (ID_MAP, NAME_MAP, TIME_MAP, LEGHT_MAP, SPEED_MAP) values (6, '2', '83-10-26 0:40:13', 'F', '5');
+
+insert into MAP (ID_MAP, NAME_MAP, TIME_MAP, LEGHT_MAP, SPEED_MAP) values (2, '3', '629-5-23 4:35:8', 'G', '6');
+
+insert into MAP (ID_MAP, NAME_MAP, TIME_MAP, LEGHT_MAP, SPEED_MAP) values (12, '4', '1221-8-3 8:15:54', 'H', '7');
+
+insert into MAP (ID_MAP, NAME_MAP, TIME_MAP, LEGHT_MAP, SPEED_MAP) values (19, '5', '1494-11-12 10:7:24', 'I', '8');
+
+insert into MAP (ID_MAP, NAME_MAP, TIME_MAP, LEGHT_MAP, SPEED_MAP) values (4, '6', '794-4-29 6:6:29', ' ', '9');
+
+insert into MAP (ID_MAP, NAME_MAP, TIME_MAP, LEGHT_MAP, SPEED_MAP) values (17, '7', '148-6-9 1:27:14', '0', 'A');
+
+insert into MAP (ID_MAP, NAME_MAP, TIME_MAP, LEGHT_MAP, SPEED_MAP) values (18, '8', '998-12-4 7:27:46', '1', 'B');
+
+insert into INFO_DR_MAP (ID_IDM, NAME_DR, NAME_MAP) values (15, 'E', 'E');
+
+insert into INFO_DR_MAP (ID_IDM, NAME_DR, NAME_MAP) values (14, 'F', ' ');
+
+insert into INFO_DR_MAP (ID_IDM, NAME_DR, NAME_MAP) values (18, 'G', ' ');
+
+insert into INFO_DR_MAP (ID_IDM, NAME_DR, NAME_MAP) values (13, 'H', ' ');
+
+insert into INFO_DR_MAP (ID_IDM, NAME_DR, NAME_MAP) values (0, 'I', ' ');
+
+insert into INFO_DR_MAP (ID_IDM, NAME_DR, NAME_MAP) values (19, ' ', ' ');
+
+insert into INFO_DR_MAP (ID_IDM, NAME_DR, NAME_MAP) values (1, '0', ' ');
+
+insert into INFO_DR_MAP (ID_IDM, NAME_DR, NAME_MAP) values (5, '1', ' ');
+
+insert into INFO_DR_MAP (ID_IDM, NAME_DR, NAME_MAP) values (7, '2', ' ');
+
+insert into INFO_DR_MAP (ID_IDM, NAME_DR, NAME_MAP) values (2, '3', ' ');
+
+insert into INFO_DR_MAP (ID_IDM, NAME_DR, NAME_MAP) values (17, '4', ' ');
+
+insert into INFO_DR_MAP (ID_IDM, NAME_DR, NAME_MAP) values (12, '5', ' ');
+
+insert into INFO_DR_MAP (ID_IDM, NAME_DR, NAME_MAP) values (11, '6', ' ');
+
+insert into INFO_DR_MAP (ID_IDM, NAME_DR, NAME_MAP) values (16, '7', ' ');
+
+insert into INFO_DR_MAP (ID_IDM, NAME_DR, NAME_MAP) values (4, '8', ' ');
+
+insert into INFO_DR_MAP (ID_IDM, NAME_DR, NAME_MAP) values (8, '9', ' ');
+
+insert into INFO_DR_MAP (ID_IDM, NAME_DR, NAME_MAP) values (3, 'A', ' ');
+
+insert into INFO_DR_MAP (ID_IDM, NAME_DR, NAME_MAP) values (9, 'B', ' ');
+
+insert into INFO_DR_MAP (ID_IDM, NAME_DR, NAME_MAP) values (10, 'C', ' ');
+
+insert into INFO_DR_MAP (ID_IDM, NAME_DR, NAME_MAP) values (6, 'D', ' ');
+
